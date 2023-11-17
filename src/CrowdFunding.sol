a98a81143b978795a16e5ae4296cdbb6700037fb
shabs21 <shabs2196@gmail.com>
2023-11-17 16:30:17 +0530

    crowdFunding example

---
+++ src/CrowdFunding.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract CrowdFunding {
+	uint256 public number;
+	address public _owner;
+	IERC20 public _asset;
+
+	constructor(address owner, IERC20 asset) {
+		_owner = owner;
+		_asset = asset;
+	}
+
+	function setNumber(uint256 newNumber) public {
+		number = newNumber;
+	}
+
+	function increment() public {
+		number++;
+	}
+
+	function deposit(uint256 _amount) public {
+		_asset.transferFrom(msg.sender, address(this), _amount);
+	}
+
+	function withdraw() public {
+		require(_owner == msg.sender, "not Owner");
+        uint256 amount = _asset.balanceOf(address(this));
+        // _asset.approve(msg.sender, amount);
+		_asset.transferFrom(address(this), msg.sender, amount);
+	}
+}
---
+++ test/Counter.t.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {CrowdFunding} from "../src/CrowdFunding.sol";
+import {SimpleToken} from "../src/SimpleToken.sol";
+
+contract CrowdFundingTest is Test {
+	CrowdFunding public crowdFunding;
+	address john = makeAddr("john");
+	SimpleToken public token;
+
+	function setUp() public {
+		token = new SimpleToken(1000);
+		crowdFunding = new CrowdFunding(msg.sender, token);
+		crowdFunding.setNumber(0);
+	}
+
+	function test_Increment() public {
+		crowdFunding.increment();
+		assertEq(crowdFunding.number(), 1);
+	}
+
+	function testFuzz_SetNumber(uint256 x) public {
+		crowdFunding.setNumber(x);
+		assertEq(crowdFunding.number(), x);
+	}
+
+	function test_ownerCheck() public {
+		assertEq(crowdFunding._owner(), msg.sender);
+	}
+
+	function test_depositCheck() public {
+		token.approve(address(crowdFunding), 10);
+		crowdFunding.deposit(10);
+		assertEq(token.balanceOf(address(crowdFunding)), 10);
+
+		vm.startPrank(john);
+		token.mint(john, 10);
+		token.approve(address(crowdFunding), 10);
+		crowdFunding.deposit(10);
+		vm.stopPrank();
+		assertEq(token.balanceOf(address(crowdFunding)), 20);
+		console2.log(token.balanceOf(address(crowdFunding)));
+	}
+
+	function test_withdrawCheck() public {
+		// token.approve(address(crowdFunding), 10);
+		// crowdFunding.deposit(10);
+		// assertEq(token.balanceOf(address(crowdFunding)), 10);
+
+		vm.startPrank(john);
+		token.mint(john, 10);
+		token.approve(address(crowdFunding), 10);
+		crowdFunding.deposit(10);
+		vm.stopPrank();
+		// assertEq(token.balanceOf(address(crowdFunding)), 20);
+		console2.log(msg.sender);
+		console2.log(crowdFunding._owner());
+
+		vm.prank(address(crowdFunding));
+		token.approve(msg.sender, 10);
+		vm.prank(msg.sender);
+		crowdFunding.withdraw();
+
+		assertEq(token.balanceOf(msg.sender), 10);
+	}
+}
